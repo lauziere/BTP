b8b248230f72e1bb59adda427ae57f0ea77baef3
lauziere <39139279+lauziere@users.noreply.github.com>
2021-11-10 18:45:33 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import scipy.sparse as sp
+import time
+
+from mbest import *
+
+if __name__ == '__main__':
+	
+	in_vals = np.random.randn(100,3) + 10
+	out_vals = np.random.randn(100,3) + 12
+
+	N = in_vals.shape[0]
+	M = out_vals.shape[0]
+	d = N*(M+1)
+
+	rad = 1
+
+	A, Aeq, b, beq = build_initial_constr(N, M)
+
+	f = build_C(in_vals, out_vals, rad)
+
+	K = 50
+	x, xv = MBest(f, A, b, Aeq, beq, K)
+
+	print(xv)
+
+
